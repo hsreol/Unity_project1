76b4f5eefe6b1d176dc8f6b4c73915570c8dd46e
hsreol <33413830+hsreol@users.noreply.github.com>
2021-06-19 21:40:38 +0900

    Add files via upload

---
+++ light_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_controller : MonoBehaviour
+{
+
+    public void Sun_control1()
+    {
+        RenderSettings.ambientIntensity = 0.3f;
+        this.transform.localEulerAngles = new Vector3(15, -15, -75);
+    }
+
+    public void Sun_control2()
+    {
+        RenderSettings.ambientIntensity = 1.1f;
+
+
+        this.transform.localEulerAngles = new Vector3(160, 90, 210);
+
+
+    }
+
+    public void Sun_control3()
+    {
+        RenderSettings.ambientIntensity = 0.5f;
+        this.transform.localEulerAngles = new Vector3(160, 55, 55);
+    }
+}
